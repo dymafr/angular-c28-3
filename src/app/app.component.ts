0e63f2c37ccc393789ace77419041522a642ba27
dymafr <erwan1309@hotmail.com>
2021-10-03 18:31:18 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Todo } from './shared/interfaces/todo.interface';
 import {
-  addTodoAction,
-  deleteTodoAction,
+  tryDeleteTodoAction,
+  tryUpdateTodoAction,
   fetchTodosAction,
-  toggleTodoAction,
+  tryAddTodoAction,
 } from './shared/store/todos.actions';
 import { selectTodosData } from './shared/store/todos.selectors';
 
@@ -19,23 +19,24 @@ export class AppComponent {
   public todos$: Observable<Todo[]> = this.store.select(selectTodosData);
   public message!: string;
 
+  constructor(private store: Store) {}
+
   ngOnInit() {
     this.store.dispatch(fetchTodosAction());
   }
 
-  constructor(private store: Store) {}
-
   public addTodo() {
     this.store.dispatch(
-      addTodoAction({ todo: { message: this.message, done: false } })
+      tryAddTodoAction({ todo: { message: this.message, done: false } })
     );
   }
 
-  public toggleTodo(index: number) {
-    this.store.dispatch(toggleTodoAction({ index }));
+  public updateTodo(todo: Todo) {
+    const newTodo = { ...todo, done: !todo.done };
+    this.store.dispatch(tryUpdateTodoAction({ todo: newTodo }));
   }
 
-  public deleteTodo(index: number) {
-    this.store.dispatch(deleteTodoAction({ index }));
+  public deleteTodo(todo: Todo) {
+    this.store.dispatch(tryDeleteTodoAction({ todo }));
   }
 }
