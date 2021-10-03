e7f16fba4348400f6a46c384cf00e9470396115e
dymafr <erwan1309@hotmail.com>
2021-10-03 18:19:34 +0200

    2 files modified

--- src/app/shared/interfaces/todo.interface.ts
+++ src/app/shared/interfaces/todo.interface.ts
@@ -1,4 +1,5 @@
 export interface Todo {
+  _id?: string;
   message: string;
   done: boolean;
 }
--- src/app/shared/services/todos.service.ts
+++ src/app/shared/services/todos.service.ts
@@ -12,4 +12,20 @@ export class TodosService {
   public fetchTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>('https://restapi.fr/api/ngrxtodos');
   }
+
+  public updateTodo(todo: Todo): Observable<Todo> {
+    const { _id, ...newTodo } = todo;
+    return this.http.patch<Todo>(
+      `https://restapi.fr/api/ngrxtodos/${todo._id}`,
+      newTodo
+    );
+  }
+
+  public deleteTodo(todo: Todo): Observable<any> {
+    return this.http.delete(`https://restapi.fr/api/ngrxtodos/${todo._id}`);
+  }
+
+  public addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>('https://restapi.fr/api/ngrxtodos', todo);
+  }
 }
